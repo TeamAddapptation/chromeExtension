53a27f60db9d12154f181b031c7d8fa540c06663
Jason Bean <jasonbean92@gmail.com>
2020-11-13 09:43:22 -0500

    Design feedback R1

--- addapptation.js
+++ addapptation.js
@@ -8,6 +8,25 @@ function time(){
 time();
 setInterval(time,1000);
 /*---------------------------------------------
+Background Image
+---------------------------------------------*/
+const numItemsToGenerate = 20; //how many gallery items you want on the screen
+const numImagesAvailable = 242; //how many total images are in the collection you are pulling from
+const imageWidth = 1200; //your desired image width in pixels
+const imageHeight = 1200; //desired image height in pixels
+const collectionID = 162468; //the collection ID from the original url
+
+function renderGalleryItem(randomNumber){
+  fetch(`https://source.unsplash.com/collection/${collectionID}/${imageWidth}x${imageHeight}/?sig=${randomNumber}`)
+    .then((response)=> {
+      let tabWrapper = document.getElementById('a__ce_wrapper')
+      tabWrapper.style.backgroundImage = `url('${response.url}')`;
+    })
+}
+
+let randomImageIndex = Math.floor(Math.random());
+renderGalleryItem(randomImageIndex);
+/*---------------------------------------------
 Quote
 ---------------------------------------------*/
 const url = "https://api.quotable.io/random";
